cc3f071373286290f11a2b96b755c667c934ab43
Sheldan <5037282+Sheldan@users.noreply.github.com>
2024-01-22 02:30:00 +0100

    deleting target folder (again)

--- gw2-tools-backend/database/target/classes/changeLog/0.0.1/tables/sql/insert_trigger.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE FUNCTION insert_trigger_procedure() RETURNS trigger
-LANGUAGE plpgsql
-AS $$
-    BEGIN
-    NEW.created := CURRENT_TIMESTAMP;
-    RETURN NEW;
-    END;
-$$;
--- gw2-tools-backend/database/target/classes/changeLog/0.0.1/tables/sql/update_trigger.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE FUNCTION update_trigger_procedure() RETURNS trigger
-LANGUAGE plpgsql
-AS $$
-    BEGIN
-    NEW.updated := CURRENT_TIMESTAMP;
-    RETURN NEW;
-    END;
-$$;
